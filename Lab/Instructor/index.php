888d2c3dc1de9a528b1ade28ce212d5f5ef1a571
Daubin <druesche@asu.edu>
2016-11-07 11:34:18 -0700

    Added inputs for test cases

--- Lab/Instructor/index.php
+++ Lab/Instructor/index.php
@@ -14,43 +14,125 @@ $steps = $lab->getSteps();
 
 //TODO: Implement AJAX to Create "Delete" Buttons on Client Side for Steps
 
+//TODO: Add a 3 X 10 table, create input and output test cases
+
 ?>
 <HTML>
-    <header>
-        <title>Instructor View</title>
-        <link rel="stylesheet" type="text/css" href="style.css" >
-    </header>
-    <body>
-        <h1><?php echo $_SESSION['context_label'] . ": " . $_SESSION['context_title'] . " Lab System" ?></h1>
-        <h2>Welcome <?php echo $_SESSION['lis_person_name_given'];  ?>!</h2>
-        <hr />
-        <div class="lab-window">
-            <div class="lab-window-header">
-                <h1 class="lab-title"><?php echo $_SESSION['resource_link_title']; ?> </h1>
-            </div>
-            <div class="step-window options-window">
-                <p>Lab Settings</p>
-                <form action="save-lab.php" method="post">
-                    <label for="due_date">Due Date</label>
-                    <input type="datetime-local" name="due_date" >
-                    <label for="open_date">Open Date</label>
-                    <input type="datetime-local" name="open_date" >
-                    <input type="submit" value ="Save">
-                </form>
-            </div>
-            <?php
-            if(!is_null($steps))
-            {
-                foreach ($steps as $step)
-                {
-                    echo "<div class='step-window'><p><a href='edit-step.php?step=" . $step->GetStepID() ."'>Step ". $step->GetStepMask() . "</a></p><p>" . $step->GetInstructions() ."</p><div class='delete'><a  href='step-delete.php?step=" . $step->GetStepID() .  "'>X</a></div> </div>";
-                }
-            }
-            ?>
-            <div class="step-window add-step">
-                <a href="add-step.php">Add Step</a>
-            </div>
-            <a href="error-console.php">Error Console</a>
-        </div>
-    </body>
+<header>
+    <title>Instructor View</title>
+    <link rel="stylesheet" type="text/css" href="style.css" >
+</header>
+<body>
+<h1><?php echo $_SESSION['context_label'] . ": " . $_SESSION['context_title'] . " Lab System" ?></h1>
+<h2>Welcome <?php echo $_SESSION['lis_person_name_given'];  ?>!</h2>
+<hr />
+<div class="lab-window">
+    <div class="lab-window-header">
+        <h1 class="lab-title"><?php echo $_SESSION['resource_link_title']; ?> </h1>
+    </div>
+    <div class="step-window options-window">
+        <p>Lab Settings</p>
+        <form action="save-lab.php" method="post" enctype="multipart/form-data">
+            <label for="open_date">Open Date</label>
+            <input type="datetime-local" name="open_date" >
+            <label for="due_date">Due Date</label>
+            <input type="datetime-local" name="due_date" >
+            <table style="margin-top: 0.8rem">
+                <tr>
+                    <th><!-- to be left blank as placeholder --></th>
+                    <th>Input Test Cases (.txt)</th>
+                    <th>Output Test Cases (.txt)</th>
+                </tr>
+                <tr>
+                    <td>1: </td>
+                    <td><input type= "file" name = "inputFile[]"/></td>
+                    <td><input type= "file" name = "outputFile[]"/></td>
+                </tr>
+                <tr>
+                    <td>2: </td>
+                    <td><input type= "file" name = "inputFile[]"/></td>
+                    <td><input type= "file" name = "outputFile[]"/></td>
+                </tr>
+                <tr>
+                    <td>3: </td>
+                    <td><input type= "file" name = "inputFile[]"/></td>
+                    <td><input type= "file" name = "outputFile[]"/></td>
+                </tr>
+                <tr>
+                    <td>4: </td>
+                    <td><input type= "file" name = "inputFile[]"/></td>
+                    <td><input type= "file" name = "outputFile[]"/></td>
+                </tr>
+                <tr>
+                    <td>5: </td>
+                    <td><input type= "file" name = "inputFile[]"/></td>
+                    <td><input type= "file" name = "outputFile[]"/></td>
+                </tr>
+                <tr>
+                    <td>6: </td>
+                    <td><input type= "file" name = "inputFile[]"/></td>
+                    <td><input type= "file" name = "outputFile[]"/></td>
+                </tr>
+                <tr>
+                    <td>7: </td>
+                    <td><input type= "file" name = "inputFile[]"/></td>
+                    <td><input type= "file" name = "outputFile[]"/></td>
+                </tr>
+                <tr>
+                    <td>8: </td>
+                    <td><input type= "file" name = "inputFile[]"/></td>
+                    <td><input type= "file" name = "outputFile[]"/></td>
+                </tr>
+                <tr>
+                    <td>9: </td>
+                    <td><input type= "file" name = "inputFile[]"/></td>
+                    <td><input type= "file" name = "outputFile[]"/></td>
+                </tr>
+                <tr>
+                    <td>10: </td>
+                    <td><input type= "file" name = "inputFile[]"/></td>
+                    <td><input type= "file" name = "outputFile[]"/></td>
+                </tr>
+            </table>
+            <input type="submit" value ="Save Lab" style="margin-top: 0.8rem">
+        </form>
+    </div>
+    <?php
+    if(!is_null($steps))
+    {
+        foreach ($steps as $step)
+        {
+            echo "<div class='step-window'><p><a href='edit-step.php?step=" . $step->GetStepID() ."'>Step ". $step->GetStepMask() . "</a></p><p>" . $step->GetInstructions() ."</p><div class='delete'><a  href='step-delete.php?step=" . $step->GetStepID() .  "'>X</a></div> </div>";
+        }
+    }
+    if(isset($_FILES['inputFile'])){
+        //Example: script to grab for first file of input files array
+        //$file_name = $_FILES['inputFile']['name'][0];
+        //$file_size =$_FILES['inputFile']['size'][0];
+        //$file_type=$_FILES['inputFile']['type'][0];
+        //$file_ext=strtolower(end(explode('.',$_FILES['inputFile']['name'][0])));
+
+        $errors= array();
+        $extensions= array("txt");
+
+        if(in_array($file_ext,$extensions)=== false){
+            $errors[]="extension not allowed, please choose a .txt file.";
+        }
+
+        if(empty($errors)==true){
+            //This is where we can upload to database
+            //move_uploaded_file($file_tmp,"files/".$file_name);
+            echo "Success";
+        }else{
+            print_r($errors);
+        }
+    }
+
+    ?>
+    <div class="step-window add-step">
+        <a href="add-step.php">Add Step</a>
+    </div>
+    <a href="error-console.php">Error Console</a>
+</div>
+</body>
 </HTML>
