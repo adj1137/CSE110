ac54610360fde42e2a240cac73194852d3879c0d
Allen James <Adjames5@assu.edu>
2016-11-07 12:22:05 -0700

    Added Input Stream Functionality to Compilation.

---
+++ Lab/Compile/test.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Allen
+ * Date: 11/7/2016
+ * Time: 11:29 AM
+ */
+
+include "JavaHandler.php";
+
+$code = "import java.util.*; 
+        public class Test {
+            public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            System.out.println(sc.nextLine());
+            }
+        }";
+$java = new JavaHandler($code);
+
+$java->Compile();
+
+$java->Run("input1.txt");
+
+echo $java->GetOutput();
+
