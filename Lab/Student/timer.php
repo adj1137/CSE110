6fcf19846769d2d82e889db683dbcda569469cdd
Allen James <Adjames5@assu.edu>
2016-11-14 11:09:39 -0700

    Added Timer File

--- Lab/Student/timer.php
+++ Lab/Student/timer.php
@@ -6,14 +6,15 @@ session_start();
  * Date: 11/14/2016
  * Time: 10:56 AM
  */
+include_once '../Lab/Include/functions.php';
 include "../Model/Database.php";
 
 if(CheckTimer($_SESSION['resource_link_id'], $_SESSION['user_id']))
 {
-    Redirect('../labview.php');
+    Redirect('../Student/labview.php');
 }
 else
 {
     StartTimer($_SESSION['resource_link_id'], $_SESSION['user_id']);
-    Redirect('../labview.php');
+    Redirect('../Student/labview.php');
 }
