5bba6f9309adde3d65985e0cf5f55157d52b2e01
Allen James <Adjames5@assu.edu>
2016-11-14 11:13:10 -0700

    Fixed Import in Timer File

--- Lab/Student/timer.php
+++ Lab/Student/timer.php
@@ -6,7 +6,7 @@ session_start();
  * Date: 11/14/2016
  * Time: 10:56 AM
  */
-include_once '../Lab/Include/functions.php';
+include_once '../Include/functions.php';
 include "../Model/Database.php";
 
 if(CheckTimer($_SESSION['resource_link_id'], $_SESSION['user_id']))
