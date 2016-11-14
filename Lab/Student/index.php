fcdeb94e1725ec94d885f2e5abe61160ab00597c
Allen James <Adjames5@assu.edu>
2016-11-14 10:55:20 -0700

    Changed Student Index

--- Lab/Student/index.php
+++ Lab/Student/index.php
@@ -1,7 +1,22 @@
 <?php
+session_start();
 /**
  * Created by PhpStorm.
  * User: Allen
  * Date: 11/14/2016
  * Time: 10:48 AM
  */
+?>
+<HTML>
+<header>
+    <title>Student Lab Portal</title>
+</header>
+<body>
+<h1><?php echo $_SESSION['resource_link_title']; ?></h1>
+<h1>Welcome <?php echo $_SESSION['lis_person_name_given'] ?>!</h1>
+<h2></h2>
+    <form method="get" action="timer.php">
+        <button type="submit" onclick="timer.php" value="Begin Lab" />
+    </form>
+</body>
+</HTML>
