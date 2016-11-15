4cdf957f3f7408a94ced28c6ee4dda853293d05e
Allen James <Adjames5@assu.edu>
2016-11-14 21:54:59 -0700

    Work on Add Step

--- Lab/Instructor/add-step.php
+++ Lab/Instructor/add-step.php
@@ -1,29 +1,19 @@
 <?php session_start();
-
 include_once '../Include/functions.php';
 include_once '../Model/Step.php';
-
 /**
  * Created by PhpStorm.
  * User: Allen James
  * Date: 9/1/2016
  * Time: 7:18 PM
  */
-
 echo "<pre>";
 echo var_dump($_SESSION);
 echo "</pre>";
-
 include_once '../Model/Lab.php';
-
 $lab = new Lab($_SESSION['resource_link_id']);
-
 $step = $lab->addStep();
-
 $id = $step->getStepID();
-
 //This is not the place to do this!!! This causes problems!
 //$step->Save();
-
 Redirect('../Instructor/edit-step.php?step=' . $id);
-
