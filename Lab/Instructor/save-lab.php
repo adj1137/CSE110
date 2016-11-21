bfb313cdf4244ee18de1808ba62161836252f586
Daubin <druesche@asu.edu>
2016-11-21 13:26:59 -0700

    Added files functionality

--- Lab/Instructor/save-lab.php
+++ Lab/Instructor/save-lab.php
@@ -15,8 +15,44 @@ if(isset($_POST['save']))
 
     $Lab->save();
 
-    Redirect("index.php");
+    //TODO: Remove comment lines after testing
+    //Redirect("index.php");
 
+    $input = $_FILES['inputFile'];
+    $output = $_FILES['outputFile'];
+
+    
+    //Traverse inputs and if not empty store input and corresponding output in compile folder
+    $i = 0;
+    while(strcmp($input['name'][$i], "") != 0){
+        $path_in = $_SERVER['DOCUMENT_ROOT'] . "/CSE110/Lab/Compile/" . $_SESSION['resource_link_id'] . "/" . "input/";
+        $path_out = $_SERVER['DOCUMENT_ROOT'] . "/CSE110/Lab/Compile/" . $_SESSION['resource_link_id'] . "/" . "output/";
+        if (!file_exists($path_in)) {
+            mkdir($path_in, 0777, true);
+        }
+        if (!file_exists($path_out)) {
+            mkdir($path_out, 0777, true);
+        }
+        move_uploaded_file($input['tmp_name'][$i], $path_in . "in". $i . ".txt");
+        move_uploaded_file($output['tmp_name'][$i], $path_out . "out". $i . ".txt");
+        $i++;
+    }
+}
+
+function reArrayFiles($file)
+{
+    $file_ary = array();
+    $file_count = count($file['name']);
+    $file_key = array_keys($file);
+
+    for($i=0;$i<$file_count;$i++)
+    {
+        foreach($file_key as $val)
+        {
+            $file_ary[$i][$val] = $file[$val][$i];
+        }
+    }
+    return $file_ary;
 }
 
 ?>
