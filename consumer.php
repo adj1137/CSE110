01930daaca15df2a56a314e7aa7fdd42a2d27d53
Allen James <Adjames5@assu.edu>
2016-11-23 13:34:46 -0700

    Modified Consumer Example For Demonstration

--- consumer.php
+++ consumer.php
@@ -12,6 +12,7 @@
 <a href="blti-launch.php" target="iframe">Instructor Lab 1</a>
 <a href="blti-launch-student2.php" target="iframe">Student Lab 2</a>
 <a href="blti-launch2.php" target="iframe">Instructor Lab 2</a>
-<iframe name="iframe" src="" height="800" width="800" ></iframe>
+<br>
+<iframe name="iframe" src="" height="600" width="800" ></iframe>
 </body>
 </html>
